42d6033a344c9153e8530e335a6c3ea6940e350b
trogdor the burninator <joe@rconjoe.com>
2020-07-02 13:29:39 -0400

    -y

--- gsm-sh.sh
+++ gsm-sh.sh
@@ -6,8 +6,8 @@ cd gs
 
 echo "Installing dependencies..."
 sudo apt update
-sudo apt install tuned htop
-sudo dpkg --add-architecture i386; sudo apt update; sudo apt install mailutils postfix curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 lib32stdc++6 steamcmd
+sudo apt -y install tuned htop
+yes | sudo dpkg --add-architecture i386; sudo apt update; sudo apt install mailutils postfix curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 lib32stdc++6 steamcmd
 
 echo "Downloading LinuxGSM - eFire s3"
 wget -O linuxgsm.sh https://raw.githubusercontent.com/rconjoe/LinuxGSM/master/linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh csgoserver
