399b61c311128adb98396ff42c704928d5bacf2c
trogdorgg <jcarlton1227@gmail.com>
2020-07-02 13:06:17 -0400

    init

---
+++ gsm-sh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir gs
+cd gs
+sudo apt update
+sudo apt install tuned htop
+sudo dpkg --add-architecture i386; sudo apt update; sudo apt install mailutils postfix curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 lib32stdc++6 steamcmd
+wget -O linuxgsm.sh https://raw.githubusercontent.com/rconjoe/LinuxGSM/master/linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh csgoserver
+./csgoserver auto-install
+
+echo "Upon successful install, run ./csgoserver ul to add your packages to the mod-install list."
