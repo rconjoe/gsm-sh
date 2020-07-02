508db89db23b895a359032be59a2deca9816d965
trogdor the burninator <joe@rconjoe.com>
2020-07-02 13:39:59 -0400

    spacing error

--- gsm-sh.sh
+++ gsm-sh.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt -y install tuned htop
 yes | sudo dpkg --add-architecture i386; sudo apt update; sudo apt -y install mailutils postfix curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 lib32stdc++6 steamcmd
 
-echo -e"\n \n \n Downloading LinuxGSM - eFire s3\n \n \n"
+echo -e "\n \n \n Downloading LinuxGSM - eFire s3\n \n \n"
 wget -O linuxgsm.sh https://raw.githubusercontent.com/rconjoe/LinuxGSM/master/linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh csgoserver
 ./csgoserver auto-install
 
